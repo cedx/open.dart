f53e54362d1f7d0c5c7dd8f9870f23654403248c
Cédric Belin <cedric@belin.io>
2019-10-11 19:35:27 +0200

    Implemented the example code

--- example/main.dart
+++ example/main.dart
@@ -2,6 +2,20 @@
 import 'package:open/open.dart';
 
 /// Demonstrates the usage of the `open()` function.
-void main() {
-  // TODO
+Future<void> main() async {
+  // Open a URL in the default browser.
+  await open('https://belin.io');
+
+  // Open a URL in the given browser.
+  await open('https://belin.io', application: 'firefox');
+
+  // Open a URL in the given browser, using the specified arguments.
+  await open('https://belin.io',
+    application: 'chrome',
+    arguments: ['--incognito']
+  );
+
+  // Open an image in the default viewer
+  // and wait for the opened application to quit.
+  await open('funny.gif', wait: true);
 }
