b533b45667110a0734b607430874ce311a5b7359
Cédric Belin <cedric@belin.io>
2020-06-08 19:28:42 +0200

    Updated the build system

--- tool/test.ps1
+++ tool/test.ps1
@@ -3,5 +3,5 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 Write-Host "The test assertions must be verified manually as there is no way to check that they actually opened anything."
-pub run build_runner build --delete-conflicting-outputs
+tool/build.ps1
 pub run test --coverage=var
